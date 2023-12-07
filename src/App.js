f9fb1abed993057ad327636ae1254e88d7f1ef2a
Mariya Hadzhiyska <136964468+DevMarieta@users.noreply.github.com>
2023-12-07 09:37:18 +0200

    publik

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import { UserContextProvider } from "./components/UserContext";
+
+function App() {
+  return <UserContextProvider> </UserContextProvider>;
+}
+
+export default App;
